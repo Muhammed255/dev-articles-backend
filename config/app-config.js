8017bd3d360fbfe9fb2c470506ee75dcb003d95e
Muhammed255 <mhmd.ahmd.0597@gmail.com>
2024-02-09 01:56:20 +0200

    fix db connection

--- config/app-config.js
+++ config/app-config.js
@@ -1,9 +1,19 @@
-// mongodb+srv://Mohamed:fhyMFsYzNPxgs692@cluster0.t278n.mongodb.net/developers_articles?retryWrites=true&w=majority
+import "dotenv/config";
+
+const {
+	JWT_SECRET,
+	DB_URL,
+	CLOUDINARY_API_NAME,
+	CLOUDINARY_API_KEY,
+	CLOUDINARY_API_SECRET,
+	BACKEND_URL,
+} = process.env;
+
 export const appConfig = {
-  JWT_SECRET: "$#GR24T4344$#$@#%WTEWTEAE%$6",
-  DATABASE_URL: 'mongodb+srv://Mhmd:cs6kwkD7aduEmSZ@cluster0.8xdx0.mongodb.net/dev-articles?retryWrites=true&w=majority',
-  BACKEND_URL: "http://localhost/3000",
-  CLOUDINARY_CLOUD_API_NAME: "dywgzxh2f",
-  CLOUDINARY_CLOUD_API_KEY: "111678333123264",
-  CLOUDINARY_CLOUD_API_SECRET: "LkholL6S-hY-_tK-aDEZ0-GP6PY",
+	JWT_SECRET: JWT_SECRET,
+	DATABASE_URL: DB_URL,
+	BACKEND_URL: BACKEND_URL,
+	CLOUDINARY_CLOUD_API_NAME: CLOUDINARY_API_NAME,
+	CLOUDINARY_CLOUD_API_KEY: CLOUDINARY_API_KEY,
+	CLOUDINARY_CLOUD_API_SECRET: CLOUDINARY_API_SECRET,
 };
