d00ef5b19a295378e6f425cd392354206a7338ac
Muhammed255 <mhmd.ahmd.0597@gmail.com>
2024-10-16 16:57:41 +0300

    back to mongo db setup 2

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ import compression from 'compression';
 import cors from "cors";
 import express from 'express';
 import mongoose from 'mongoose';
-import serverless from 'serverless-http'
 
 import { appConfig } from './config/app-config.js';
-import { articlePostRoutes } from './routes/article-post.routes.js';
-import { authRouter } from './routes/auth.routes.js';
-import { categoryRoutes } from './routes/category.routes.js';
-import { topicRoutes } from './routes/topic.routes.js';
 import routes from './routes/routes.js'
 const app = express();
-
-const port = process.env.PORT || "3000";
 
 app.use(compression());
 
