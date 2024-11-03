b04f5017edb287ab5ee32d0bbb799717aff22e6e
Muhammed255 <mhmd.ahmd.0597@gmail.com>
2024-11-03 22:09:43 +0200

    finalize 1

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,10 +3,23 @@ import { appConfig } from "../config/app-config.js";
 
 export function checkAuth(req, res, next) {
   try {
-		if(req.headers && !req.headers.authorization) {
-			return res.status(401).json({msg: "Token is missed"})
-		}
-    const token = req.headers.authorization.split(" ")[1];
+		const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        msg: "Authorization header is missing"
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        msg: "Token is missing"
+      });
+    }
+
     const decodedToken = jwt.verify(token, appConfig.JWT_SECRET);
     req.userData = {
       email: decodedToken.email,
--- middleware/token-update.js
+++ middleware/token-update.js
@@ -3,12 +3,15 @@ import User from "../models/user.model.js";
 
 export const tokenUpdateMiddleware = async (req, res, next) => {
   try {
-    if (req.userData) { // Assuming you have middleware that sets userData
-      const user = await User.findById(req.userData.userId);
-      if (user && user.accessToken !== req.headers.authorization?.split(' ')[1]) {
-        // If the token in the database differs from the request token
-        res.set('New-Access-Token', user.accessToken);
-      }
+		if (!req.userData?.userId) {
+      return next();
+    }
+    const user = await User.findById(req.userData.userId);
+    const currentToken = req.headers.authorization?.split(' ')[1];
+
+    if (user && user.accessToken && user.accessToken !== currentToken) {
+      // If the token in the database differs from the request token
+      res.set('New-Access-Token', user.accessToken);
     }
     next();
   } catch (error) {
